d17432cf38d543888efdae1effc65e0a4e74e7e7
Andreas Hagen <andreas@hagenz.com>
2020-07-24 15:36:58 +0200

    Fixed up some permissions

--- controller/role.go
+++ controller/role.go
@@ -1,8 +1,14 @@
 package controller
 
 //go:generate controller-gen rbac:roleName=istio-oidc output:dir=.
-// +kubebuilder:rbac:groups=krsdev.app,resources=accesspolicies,verbs=get;list;watch
+
+// +kubebuilder:rbac:groups=krsdev.app,resources=accesspolicies,verbs=get;list;update;watch
 // +kubebuilder:rbac:groups=krsdev.app,resources=accesspolicies/status,verbs=update
-// +kubebuilder:rbac:groups=networking.istio.io/v1alpha3,resources=gateways,verbs=get;list;watch
-// +kubebuilder:rbac:groups=networking.istio.io/v1alpha3,resources=envoyfilters,verbs=create;get;list;update;watch
+// +kubebuilder:rbac:groups=networking.istio.io,resources=gateways,verbs=get;list;watch
+// +kubebuilder:rbac:groups=networking.istio.io,resources=envoyfilters,verbs=create;get;list;update;watch
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=create;get;list;watch
+
+// Leader election
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=create;get;update
+// +kubebuilder:rbac:groups="",resources=events,verbs=create
+// TODO: Creating events don't work
