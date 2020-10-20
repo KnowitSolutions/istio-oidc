73dabdb64dd88f56ba11f529f2de98d803560bca
Andreas Hagen <andreas@hagenz.com>
2020-10-20 15:40:37 +0200

    Fixed invalid hostname making Envoy reject connections from ingress

--- controller/envoyfilter/envoyfilter.go
+++ controller/envoyfilter/envoyfilter.go
@@ -6,8 +6,13 @@ import (
 	"github.com/KnowitSolutions/istio-oidc/state/accesspolicy"
 	istionetworkingapi "istio.io/api/networking/v1alpha3"
 	istionetworking "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	"regexp"
+	"strconv"
 )
 
+var istioClusterName = regexp.MustCompile(`^.*?\|(\d+)\|(.*?)\|(.+?)$`)
+
+// TODO: Switch to new Istio ext_authz when it's ready: https://github.com/istio/istio/issues/27790
 func newEnvoyFilter(ef *istionetworking.EnvoyFilter, pols []*accesspolicy.AccessPolicy) {
 	count := 2
 	for _, pol := range pols {
@@ -15,11 +20,26 @@ func newEnvoyFilter(ef *istionetworking.EnvoyFilter, pols []*accesspolicy.Access
 	}
 	ef.Spec.ConfigPatches = make([]*istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch, 0, count)
 
+	var clusterName string
+	svc, port, subset, isIstio := parseIstioClusterName()
+	if isIstio {
+		clusterName = fmt.Sprintf("%s:%d", svc, port)
+
+		extAuthzCluster := &istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch{}
+		applyToCluster(extAuthzCluster)
+		matchCluster(extAuthzCluster, svc, port, subset)
+		merge(extAuthzCluster)
+		name(extAuthzCluster, clusterName)
+		ef.Spec.ConfigPatches = append(ef.Spec.ConfigPatches, extAuthzCluster)
+	} else {
+		clusterName = config.ExtAuthz.ClusterName
+	}
+
 	extAuthzFilter := &istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch{}
 	applyToHttpFilter(extAuthzFilter)
 	matchEnvoyRouter(extAuthzFilter)
 	insertBefore(extAuthzFilter)
-	extAuthz(extAuthzFilter)
+	extAuthz(extAuthzFilter, clusterName)
 	ef.Spec.ConfigPatches = append(ef.Spec.ConfigPatches, extAuthzFilter)
 
 	extAuthzDisable := &istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch{}
@@ -52,14 +72,32 @@ func newEnvoyFilter(ef *istionetworking.EnvoyFilter, pols []*accesspolicy.Access
 	}
 }
 
-func extAuthz(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch) {
+func parseIstioClusterName() (string, int, string, bool) {
+	match := istioClusterName.FindStringSubmatch(config.ExtAuthz.ClusterName)
+	if match == nil {
+		return "", 0, "", false
+	}
+
+	svc := match[3]
+	port, _ := strconv.Atoi(match[1])
+	subset := match[2]
+	return svc, port, subset, true
+}
+
+func name(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch, name string) {
+	patch.Patch.Value = newStruct(map[string]interface{}{
+		"name": name,
+	})
+}
+
+func extAuthz(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch, clusterName string) {
 	patch.Patch.Value = newStruct(map[string]interface{}{
 		"name": "envoy.filters.http.ext_authz",
 		"typed_config": map[string]interface{}{
 			"@type": "type.googleapis.com/envoy.config.filter.http.ext_authz.v2.ExtAuthz",
 			"grpc_service": map[string]interface{}{
 				"envoy_grpc": map[string]interface{}{
-					"cluster_name": config.ExtAuthz.ClusterName,
+					"cluster_name": clusterName,
 				},
 				"timeout": fmt.Sprintf("%.fs", config.ExtAuthz.Timeout.Seconds()),
 			},
@@ -102,6 +140,10 @@ func applyToHttpRoute(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPat
 	patch.ApplyTo = istionetworkingapi.EnvoyFilter_HTTP_ROUTE
 }
 
+func applyToCluster(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch) {
+	patch.ApplyTo = istionetworkingapi.EnvoyFilter_CLUSTER
+}
+
 func matchGateway(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch) {
 	patch.Match = &istionetworkingapi.EnvoyFilter_EnvoyConfigObjectMatch{}
 	patch.Match.Context = istionetworkingapi.EnvoyFilter_GATEWAY
@@ -133,6 +175,16 @@ func matchVirtualHostRoute(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObje
 	objectTypes := patch.Match.ObjectTypes.(*istionetworkingapi.EnvoyFilter_EnvoyConfigObjectMatch_RouteConfiguration)
 	objectTypes.RouteConfiguration.Vhost.Route = &istionetworkingapi.EnvoyFilter_RouteConfigurationMatch_RouteMatch{}
 	objectTypes.RouteConfiguration.Vhost.Route.Name = route
+}
+
+func matchCluster(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch, svc string, port int, subset string) {
+	matchGateway(patch)
+	patch.Match.ObjectTypes = &istionetworkingapi.EnvoyFilter_EnvoyConfigObjectMatch_Cluster{}
+	objectTypes := patch.Match.ObjectTypes.(*istionetworkingapi.EnvoyFilter_EnvoyConfigObjectMatch_Cluster)
+	objectTypes.Cluster = &istionetworkingapi.EnvoyFilter_ClusterMatch{}
+	objectTypes.Cluster.Service = svc
+	objectTypes.Cluster.PortNumber = uint32(port)
+	objectTypes.Cluster.Subset = subset
 }
 
 func insertBefore(patch *istionetworkingapi.EnvoyFilter_EnvoyConfigObjectPatch) {
