ca735ca41597ff2a0f08635ac05b3ab63315021c
Andreas Hagen <andreas@hagenz.com>
2020-10-20 13:35:14 +0200

    Improved logging in auth

--- auth/server.go
+++ auth/server.go
@@ -27,14 +27,14 @@ func (srv *Server) newRequest(address, cookies string, metadata map[string]strin
 
 	ap := srv.AccessPolicies.Get(metadata[accesspolicy.NameKey])
 	if ap == nil {
-		return nil, errors.New("unknown "+accesspolicy.NameKey, "AccessPolicy", metadata[accesspolicy.NameKey])
+		return nil, errors.New("unknown AccessPolicy", "AccessPolicy", metadata[accesspolicy.NameKey])
 	}
 
 	route, ok := ap.Routes[metadata[accesspolicy.RouteKey]]
 	if metadata[accesspolicy.RouteKey] == "" {
 		route = ap.Default
 	} else if !ok {
-		return nil, errors.New("unknown "+accesspolicy.RouteKey, "AccessPolicy", metadata[accesspolicy.NameKey])
+		return nil, errors.New("unknown route", "AccessPolicy", metadata[accesspolicy.NameKey])
 	}
 
 	req := http.Request{Header: http.Header{}}
